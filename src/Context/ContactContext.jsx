b36ccd48fb21e693dd81061fd55f8e5cbf033794
Gian Delmastro <gianlucadelmastro@gmail.com>
2026-07-31 15:43:37 -0300

    Arreglo de errores y nuevos contactos

--- src/Context/ContactContext.jsx
+++ src/Context/ContactContext.jsx
@@ -1,8 +1,12 @@
 import { createContext, useState } from "react";
 import { Outlet } from "react-router-dom";
-import Messi from "../assets/Messi.png";
+import MessiCopa from "../assets/MessiCopa.jpg";
 import Paredes from "../assets/Paredes.jpg";
 import Scaloni from "../assets/Scaloni.jpg";
+import DePaul from "../assets/DePaul.jpg";
+import DiMaria from "../assets/DiMaria.jpg";
+import Riquelme from "../assets/Riquelme.jpg"
+
 
 const ContactContext = createContext();
 
@@ -10,7 +14,7 @@ const server_contacts = [
     {
         id: 1,
         name: "Lionel Messi",
-        avatar: Messi,
+        avatar: MessiCopa,
         lastMessage: "Todo bien?",
         lastTime: "18:46",
         messages: [
@@ -79,6 +83,75 @@ const server_contacts = [
                 sendByMe: false,
                 content: "Aguante Boca!",
                 time: "16:16"
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Rodrigo De Paul",
+        avatar: DePaul,
+        lastMessage: "Nos vemos en la próxima fecha FIFA",
+        lastTime: "14:30",
+        messages: [
+            {
+                id: 1,
+                sendByMe: false,
+                content: "Che, como andas?",
+                time: "14:20"
+            },
+            {
+                id: 2,
+                sendByMe: true,
+                content: "Todo bien, vos?",
+                time: "14:25"
+            },
+            {
+                id: 3,
+                sendByMe: false,
+                content: "Nos vemos en la próxima fecha FIFA",
+                time: "14:30"
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "Angel Di María",
+        avatar: DiMaria,
+        lastMessage: "Como va todo?",
+        lastTime: "12:10",
+        messages: [
+            {
+                id: 1,
+                sendByMe: false,
+                content: "Buenas",
+                time: "12:05"
+            },
+            {
+                id: 2,
+                sendByMe: false,
+                content: "Como va todo?",
+                time: "12:10"
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: "Juan Román Riquelme",
+        avatar: Riquelme,
+        lastMessage: "Aguante Boca campeón",
+        lastTime: "10:05",
+        messages: [
+            {
+                id: 1,
+                sendByMe: false,
+                content: "Buen día",
+                time: "10:00"
+            },
+            {
+                id: 2,
+                sendByMe: false,
+                content: "Aguante Boca campeón",
+                time: "10:05"
             }
         ]
     }
